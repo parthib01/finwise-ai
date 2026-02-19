4a27e7cc7b30b9929eb41f15255125c60488a553
Parthib <parthibbanerjee01@gmail.com>
2026-02-19 11:29:31 +0530

    Fixed the type issue related to user_id

--- src/app/api/users.py
+++ src/app/api/users.py
@@ -4,8 +4,9 @@ from app.deps.auth import get_current_user
 router = APIRouter()
 
 @router.get("/me")
-async def me(user_id: str = Depends(get_current_user)):
+async def me(identity: dict = Depends(get_current_user)):
     return {
         "message": "You are authenticated",
-        "user_id": user_id
+        "user_id": identity["user_id"],
+        "session_id": identity["session_id"]
     }
