036ddeb9203a3f4f5a6a2ae9afd89b9a79733a80
Parthib <parthibbanerjee01@gmail.com>
2026-03-31 20:45:30 +0530

    Add logs and response structured

--- src/app/nodes/response.py
+++ src/app/nodes/response.py
@@ -1,10 +1,21 @@
 from app.services.llm_service import generate_response_llm
+from app.utils.logger import log_step
+
 
 async def generate_response(state, memory):
+    log_step("Generating Response", {
+        "intent": state.intent,
+        "db_result": state.db_result
+    })
+    
+    # Build structured payload
+    structured_data = {
+        "intent": state.intent,
+        "data": state.db_result
+    }
     response = await generate_response_llm(
         user_input=state.user_input,
-        intent=state.intent,
-        db_result=state.db_result,
+        structured_data=structured_data,
         memory=memory
     )
 
