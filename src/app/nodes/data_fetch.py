8f9cf10b034515e8322862d9fef07c1d0577504c
Parthib <parthibbanerjee01@gmail.com>
2026-03-31 20:43:33 +0530

    Add log for debugging

--- src/app/nodes/data_fetch.py
+++ src/app/nodes/data_fetch.py
@@ -1,4 +1,5 @@
 from app.services.db_service import fetch_data_from_db
+from app.utils.logger import log_step
 
 async def fetch_data(state):
     result = await fetch_data_from_db(
@@ -6,6 +7,8 @@ async def fetch_data(state):
         user_id=state.user_id,
         parameters=state.parameters
     )
+
+    log_step("DB RAW RESULT", result)
 
     state.db_result = result
     return state
